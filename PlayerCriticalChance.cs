2a2a757d706378eb9a067077bbf2bee5f296e2bf
MattyMatt357 <mc21517@my.acm.ac.uk>
2024-08-05 10:42:50 +0100

    Add project files.

---
+++ PlayerCriticalChance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CalculateDamage
+{
+    public class PlayerCriticalChance 
+    {
+        /// <summary>
+        /// Calculates the chance of dealing critical or normal damage
+        /// </summary>
+        /// <param name="randomNum"></param>
+        /// <param name="critChance"></param>
+        /// <param name="baseDamage"></param>
+        /// <param name="damageMultiplier"></param>
+        /// <returns></returns>
+        public static float WeaponDamageChance(int randomNum, int critChance, float baseDamage, float damageMultiplier)
+        {
+            if (randomNum >= critChance)
+            {
+                return baseDamage * damageMultiplier;
+            }
+            else 
+            {
+                return baseDamage;
+            }
+        }
+
+        
+    }
+}
